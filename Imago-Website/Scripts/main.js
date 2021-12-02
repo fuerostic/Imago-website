bc3f30b7b586b1f1c1eba7f5faaf6b494acf569e
=fuad <mfuad073@gmail.com>
2021-12-02 13:22:33 +0600

    adding master page and navigation scroll colour change

--- Imago-Website/Scripts/main.js
+++ Imago-Website/Scripts/main.js
@@ -2,6 +2,22 @@
 //Tools Carousel
 
 $(document).ready(function () {
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() < $(window).height()) {
+            $(".navbar").css({ "background-color": "transparent" });
+        }
+        else {
+            $(".navbar").css({ "background-color": "white" });
+        }
+
+    })
+
+    $(window).scroll(function () {
+        var $nav = $(".nav");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+
     $('.tools-carousel').owlCarousel({
         loop: true,
         margin:0,
