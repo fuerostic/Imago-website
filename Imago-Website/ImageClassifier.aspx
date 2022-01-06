bbb35d0040c166986aab25b9bb6460ff1ed0e77b
=fuad <mfuad073@gmail.com>
2022-01-06 11:37:40 +0600

    unfinished image classifier page

---
+++ Imago-Website/ImageClassifier.aspx
@@ -0,0 +1,53 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Imago.Master" AutoEventWireup="true" CodeBehind="ImageClassifier.aspx.cs" Inherits="Imago_Website.ImageClassifier" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <link href="Content/Classifier.css" rel="stylesheet" />
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+
+    <section class="home d-flex">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+
+<%--                     <h1>Meme <i>Generator</i></h1>--%>
+                    <div class="container">
+                            <img src="" alt="Add an image" id="display_image" />
+<%--                    <div id="displayImage">
+                        <p id="para">Add an image</p>
+
+                    </div>--%>
+                            
+                        </div>
+    
+ 
+                </div>
+                <div class="col-lg-6 justify-content-center">
+                    <div class=" container">
+
+                        <div class="sidebar">
+                                <p>Add Image</p>
+                                <input type="file" id="file" class="form-control" accept="image/*" onchange="detectImage()" >
+<%--                   here we left off             <button></button>--%>
+                            <div id="table">
+
+                            </div>
+                                
+                            </div>
+
+                    </div>
+
+                </div>
+
+
+            </div>
+
+        </div>
+
+    </section>
+
+
+    <script src="Scripts/ml5.min.js"></script>
+    <script src="Scripts/Classifier.js"></script>
+
+</asp:Content>
---
+++ Imago-Website/Scripts/Classifier.js
@@ -0,0 +1,91 @@
+﻿
+const image_input = document.querySelector("#file");
+const display = document.querySelector("#displayImage");
+const mytable = document.querySelector("#table");
+var classifier, label, prob;
+var uploaded_image = "";
+let headers = ['Label', 'Probability'];
+
+
+mytable.style.display = "none";
+
+preLoad();
+
+
+function preLoad() {
+    classifier = ml5.imageClassifier('MobileNet', modelLoaded);
+}
+function modelLoaded() {
+    console.log('Model Loaded!');
+}
+
+function detectImage() {
+    console.log("i am here ")
+    var reader = new FileReader();
+    reader.addEventListener("load", () => {
+        var output = document.getElementById('display_image');
+        output.src = reader.result;
+        classifier.classify(document.getElementById('display_image'), gotResults);
+    });
+    reader.readAsDataURL(event.target.files[0]);
+}
+
+// result callback function
+function gotResults(err, results) {
+  
+    mytable.style.display = "block";
+    let table = document.createElement('table');
+    let headerRow = document.createElement('tr');
+    let output = results;
+
+    console.log(output);
+    headers.forEach(headerText => {
+        let header = document.createElement('th');
+        let textNode = document.createTextNode(headerText);
+        header.appendChild(textNode);
+        headerRow.appendChild(header);
+    });
+
+    table.appendChild(headerRow);
+
+
+
+    output.forEach(result => {
+        let row = document.createElement('tr');
+
+        Object.values(result).forEach(text => {
+            let cell = document.createElement('td');
+            let textNode = document.createTextNode(text);
+            cell.appendChild(textNode);
+            row.appendChild(cell);
+        });
+        table.appendChild(row);
+    });
+
+    mytable.appendChild(table);
+}
+
+
+//image_input.addEventListener("change", function () {
+
+///*    display.src = "";*/
+
+//    const reader = new FileReader();
+//    reader.addEventListener("load", () => {
+//        uploaded_image = reader.result;
+//        display.style.backgroundImage = `url(${uploaded_image})`;
+
+//    });
+
+//    reader.readAsDataURL(this.files[0]);
+
+//    classifier = ml5.imageClassifier("MobileNet", modelReady);
+//})
+
+//function modelReady() {
+
+//    console.log(uploaded_image);
+//    classifier.predict(uploaded_image, gotResults);
+//}
+
+
