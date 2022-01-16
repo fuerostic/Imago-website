c48a33e61cf8fd6c6c559a48abf2c34425684cae
=fuad <mfuad073@gmail.com>
2022-01-16 23:28:31 +0600

    minor cleanup

--- Imago-Website/Scripts/main.js
+++ Imago-Website/Scripts/main.js
@@ -25,22 +25,22 @@ $(document).ready(function () {
 
     
 
-    $('.tools-carousel').owlCarousel({
-        loop: true,
-        margin:0,
-        responsiveClass: true,
-        responsive: {
-            0: {
-                items: 1,
-            },
-            600: {
-                items: 2,
+    //$('.tools-carousel').owlCarousel({
+    //    loop: true,
+    //    margin:0,
+    //    responsiveClass: true,
+    //    responsive: {
+    //        0: {
+    //            items: 1,
+    //        },
+    //        600: {
+    //            items: 2,
 
-            },
-            1000: {
-                items: 3,
+    //        },
+    //        1000: {
+    //            items: 3,
 
-            }
-        }
-    })
+    //        }
+    //    }
+    //})
 });
